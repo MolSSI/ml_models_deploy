f49244ffc4b4ec19051a600169c0c1607a4acd01
doaa-altarawy <doaa.altarawy@gmail.com>
2020-03-05 17:04:09 -0500

    Use MAPE and percentile as metrics from backend lib

---
+++ models/qc_time_estimator/qc_time_estimator/metrics.py
@@ -0,0 +1,10 @@
+import numpy as np
+from keras import backend
+
+
+def percentile_rel_99(y_true, y_pred):
+    err = backend.abs(y_true - y_pred) / y_true * 100
+    return np.percentile(list(err), 99)
+
+def mape(y_true, y_pred):
+    return float(backend.mean(backend.abs(y_true - y_pred) * 100 / y_true))
